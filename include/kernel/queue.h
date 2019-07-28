0ebd1bc3bcd2f8e64dd99fcd5d09037b68c32abc
Chris Fallin <cfallin@c1f.net>
2019-07-27 20:18:40 -0700

    Fixes to compile with modern-ish gcc (4.9.2) for i386.

--- include/kernel/queue.h
+++ include/kernel/queue.h
@@ -6,29 +6,29 @@
 #define _KERNEL_QUEUE_H_
 
 #define queue_insert_generic(q,i,pre) {\
-	(i)-> ## pre ## queue = (q); \
-	(i)->## pre ## next = (void *)0; \
-	(i)->## pre ## prev = (q)->tail; \
+	(i)->  pre ## queue = (q); \
+	(i)->  pre ## next = (void *)0; \
+	(i)->  pre ## prev = (q)->tail; \
 	if((q)->tail) \
-		(q)->tail->## pre ## next = (i); \
+		(q)->tail->  pre ## next = (i); \
 	else \
 		(q)->head = (i); \
 	(q)->tail = (i); \
 }
 
 #define queue_remove_generic(i,pre) {\
-	if((i)->## pre ## queue) \
+	if((i)-> pre ## queue) \
 	{ \
-		(i)->## pre ## queue->head = (i)->pre ## next; \
-		if((i)->## pre ## next) \
-			(i)->## pre ## next->## pre ## prev = (i)->## pre ## prev; \
+		(i)-> pre ## queue->head = (i)->pre ## next; \
+		if((i)-> pre ## next) \
+			(i)-> pre ## next->  pre ## prev = (i)->  pre ## prev; \
 		else \
-			(i)->## pre ## queue->tail = (i)->## pre ## prev; \
-		if((i)->## pre ## prev) \
-			(i)->## pre ## prev->## pre ## next = (i)->## pre ## next; \
+			(i)-> pre ## queue->tail = (i)->  pre ## prev; \
+		if((i)-> pre ## prev) \
+			(i)-> pre ## prev->  pre ## next = (i)->  pre ## next; \
 		else \
-			(i)->## pre ## queue->head = (i)->## pre ## next; \
-		(i)->## pre ## queue = (void *)0; \
+			(i)-> pre ## queue->head = (i)->  pre ## next; \
+		(i)-> pre ## queue = (void *)0; \
 	} \
 }
 
